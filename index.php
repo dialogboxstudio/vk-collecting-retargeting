5f3f676d715c16755314074d446d6d2a641c1ff5
Dialog Box Studio <68128870+dialogboxstudio@users.noreply.github.com>
2020-10-23 21:02:31 +0300

    Main (#1)
    * Create Retargeting.php
    
    * Create composer.json
    
    * Create composer.lock
    
    * Create .htaccess
    
    * Create index.php
    
    * Create base.json

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+use DialogBoxStudio\VkCollectingRetargeting\Retargeting;
+use Dotenv\Dotenv;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\ErrorHandler;
+use DigitalStar\vk_api\vk_api;
+use DigitalStar\vk_api\VkApiException;
+
+Dotenv::createImmutable(__DIR__, '/config/.env')->load();
+$logger = new Logger('vk-collecting-retargeting: ');
+$logger->pushHandler(new StreamHandler(__DIR__ . '/log/vk-collecting-retargeting.log'));
+ErrorHandler::register($logger);
+
+try {
+    $vk = vk_api::create($_ENV['VK_ACCESS_TOKEN'], $_ENV['VK_VERSION_API'])->setConfirm($_ENV['VK_CONFIRM']);
+} catch (VkApiException $e) {
+    $logger->critical($e);
+    exit();
+}
+
+$data = $vk->initVars($id, $message, $payload, $user_id, $type);
+
+$client = new Google_Client([
+    'application_name' => $_ENV['GOOGLE_NAME_APPLICATION'],
+    'credentials' => __DIR__.'/config/vk-collecting-retargeting-658570e5cdd8.json',
+    'scopes' => 'https://www.googleapis.com/auth/spreadsheets'
+]);
+$service = new Google_Service_Sheets($client);
+
+$retargeting =  new Retargeting($user_id, $data, $vk);
+$answer = $retargeting->getAnswer();
+
+foreach ($answer['params'] as $item => $record) {
+    $body = new Google_Service_Sheets_ValueRange( [ 'values' => $record] );
+    $options = array( 'valueInputOption' => 'RAW' );
+    $service->spreadsheets_values->update( $_ENV['GOOGLE_SPREAD_SHEET_ID'], 'Пользователи!A'.($answer['count']+1), $body, $options );
+}
+
+foreach ($answer['text'] as $item => $text) {
+    try {
+        $vk->sendMessage($user_id, $text);
+    } catch (VkApiException $e) {
+        $logger->critical($e);
+        exit();
+    }
+}
